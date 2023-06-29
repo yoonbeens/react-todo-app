813470c4bebfec895ecd97ff175635f8a35ca2c6
yoon <cunbn@naver.com>
2023-06-29 11:59:58 +0900

    프로필 이미지 관리

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -6,17 +6,23 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import { isLogin, getLoginUserInfo } from '../../util/login-utils';
 import AuthContext from '../../util/AuthContext';
+import { API_BASE_URL, USER } from '../../config/host-config';
 
 const Header = () => {
 
+    const profileRequestURL = `${API_BASE_URL}${USER}/load-profile`;
+
     const redirection = useNavigate();
+
+    // 프로필 이미지 url 상태변수
+    const [profileUrl, setProfileUrl] = useState(null);
 
 
     // AuthContext에서 로그인 상태와 onLogout 함수를 가져옵니다
     const { isLoggedIn, onLogout, userName } = useContext(AuthContext);
 
 
-    //로그아웃 핸들러
+    // 로그아웃 핸들러
     const logoutHandler = e => {
 
         // AuthContext의 onLogout 함수를 호출하여 로그인 상태를 업데이트 합니다
@@ -25,25 +31,56 @@ const Header = () => {
         redirection('/login');
     }
 
+
+    const fetchProfileImage = async() => {
+        const res = await fetch(profileRequestURL, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + localStorage.getItem('ACCESS_TOKEN') }
+        });
+
+        if(res.status === 200) {
+            // 서버에서는 바이트배열로 직렬화된 이미지가 온다
+            const profileBlob = await res.blob();
+            // 해당 이미지를 imgUrl로 변경
+            const imgUrl = window.URL.createObjectURL(profileBlob);
+            setProfileUrl(imgUrl);
+        } else {
+            const err = await res.text();
+            setProfileUrl(null);
+            // alert(err);
+        }
+
+    }
+
+    useEffect(() => {
+
+        fetchProfileImage();
+
+    }, [isLoggedIn]);
+
     
 
     return (
-        <AppBar position="fixed" style={{
-            background: '#38d9a9',
-            width: '100%'
-        }}>
+        <AppBar position="fixed" style={ {background: '#38d9a9', width: '100%'} }>
             <Toolbar>
                 <Grid justify="space-between" container>
                     <Grid item flex={9}>
-                        <div style={
-                            {
-                                display:'flex',
-                                alignItems: 'center'
-                            }
-                        }>
+                        <div style={ {display:'flex', alignItems: 'center'} }>
                             <Typography variant="h4">
                                 { isLoggedIn ? userName + '님' : '오늘' }의 할일
-                            </Typography>   
+                            </Typography>
+                            {isLoggedIn && 
+                            <img
+                                src={profileUrl || require('../../assets/img/anonymous.jpg')}
+                                alt='프사프사'
+                                style={{
+                                    marginLeft: 20,
+                                    width: 75,
+                                    height: 75,
+                                    borderRadius: '50%'
+                                }}
+                            />   
+                            }
                         </div>
                     </Grid>
 
@@ -52,8 +89,7 @@ const Header = () => {
                             {isLogin()
                                     ?
                                     (
-                                        <button className='logout-btn'
-                                        onClick={logoutHandler}>
+                                        <button className='logout-btn' onClick={logoutHandler}>
                                             로그아웃
                                         </button>
                                     )
